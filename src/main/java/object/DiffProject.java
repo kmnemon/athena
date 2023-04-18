756e9cfc2e801899221dd20423646a198f788a58
ke <ke@kes-MacBook-Pro.local>
2023-04-18 12:02:08 +0800

    add change logic

--- src/main/java/object/DiffProject.java
+++ src/main/java/object/DiffProject.java
@@ -49,12 +49,12 @@ public class DiffProject {
     public void diffTechDebtObject(){
         this.diffTechDebtObjectChange();
         this.diffTechDebtObjectOnlyIncrease();
-        this.diffDesignChange();
     }
 
     public void diffTechDebtObjectChange(){
-        this.diffMaintenanceChange();
+        this.diffMaintenanceStatisticsChange();
         this.diffRegulationChange();
+        this.diffDesignChange();
     }
 
     public void diffTechDebtObjectOnlyIncrease() {
@@ -67,17 +67,48 @@ public class DiffProject {
         this.diffDesignStatistics();
     }
 
-    private void diffMaintenanceChange(){
+    private void diffMaintenanceStatisticsChange(){
+        maintenanceChange.maintenanceStatistics.godClassesCount = target.maintenance.maintenanceStatistics.godClassesCount - base.maintenance.maintenanceStatistics.godClassesCount;
+        maintenanceChange.maintenanceStatistics.maxGodClassWithVariables = target.maintenance.maintenanceStatistics.maxGodClassWithVariables - base.maintenance.maintenanceStatistics.maxGodClassWithVariables;
+        maintenanceChange.maintenanceStatistics.maxGodClassWithMethods = target.maintenance.maintenanceStatistics.maxGodClassWithMethods - base.maintenance.maintenanceStatistics.maxGodClassWithMethods;
+        maintenanceChange.maintenanceStatistics.medianGodClassWithVariables = target.maintenance.maintenanceStatistics.maxGodClassWithVariables - base.maintenance.maintenanceStatistics.maxGodClassWithVariables;
+        maintenanceChange.maintenanceStatistics.medianGodClassWithMethods = target.maintenance.maintenanceStatistics.maxGodClassWithMethods - base.maintenance.maintenanceStatistics.maxGodClassWithMethods;
+
+        maintenanceChange.maintenanceStatistics.superMethodsCount = target.maintenance.maintenanceStatistics.superMethodsCount - base.maintenance.maintenanceStatistics.superMethodsCount;
+        maintenanceChange.maintenanceStatistics.maxSuperMethodWithParameters = target.maintenance.maintenanceStatistics.maxSuperMethodWithParameters - base.maintenance.maintenanceStatistics.maxSuperMethodWithParameters;
+        maintenanceChange.maintenanceStatistics.maxSuperMethodWithLines = target.maintenance.maintenanceStatistics.maxSuperMethodWithLines - base.maintenance.maintenanceStatistics.maxSuperMethodWithLines;
+        maintenanceChange.maintenanceStatistics.medianSuperMethodWithParameters = target.maintenance.maintenanceStatistics.maxSuperMethodWithParameters - base.maintenance.maintenanceStatistics.maxSuperMethodWithParameters;
+        maintenanceChange.maintenanceStatistics.medianSuperMethodWithLines = target.maintenance.maintenanceStatistics.maxSuperMethodWithLines - base.maintenance.maintenanceStatistics.maxSuperMethodWithLines;
+
+        maintenanceChange.maintenanceStatistics.godCommentsCount = target.maintenance.maintenanceStatistics.godCommentsCount - base.maintenance.maintenanceStatistics.godCommentsCount;
+        maintenanceChange.maintenanceStatistics.maxGodComment = target.maintenance.maintenanceStatistics.maxGodComment - base.maintenance.maintenanceStatistics.maxGodComment;
+        maintenanceChange.maintenanceStatistics.medianGodComment = target.maintenance.maintenanceStatistics.maxGodComment - base.maintenance.maintenanceStatistics.maxGodComment;
+
+        maintenanceChange.maintenanceStatistics.superCyclomaticsCount = target.maintenance.maintenanceStatistics.superCyclomaticsCount = base.maintenance.maintenanceStatistics.superCyclomaticsCount;
+        maintenanceChange.maintenanceStatistics.maxCyclomatic = target.maintenance.maintenanceStatistics.maxCyclomatic - base.maintenance.maintenanceStatistics.maxCyclomatic;
+        maintenanceChange.maintenanceStatistics.medianCyclomatic = target.maintenance.maintenanceStatistics.maxCyclomatic - base.maintenance.maintenanceStatistics.maxCyclomatic;
+
+        maintenanceChange.maintenanceStatistics.superDuplicationsCount = target.maintenance.maintenanceStatistics.superDuplicationsCount - base.maintenance.maintenanceStatistics.superDuplicationsCount;
+        maintenanceChange.maintenanceStatistics.maxDuplication = target.maintenance.maintenanceStatistics.maxDuplication - base.maintenance.maintenanceStatistics.maxDuplication;
+        maintenanceChange.maintenanceStatistics.medianDuplication = target.maintenance.maintenanceStatistics.maxDuplication - base.maintenance.maintenanceStatistics.maxDuplication;
 
     }
 
     private void diffRegulationChange(){
-
+        regulationChange.regulationStatistics.commentsCount = target.regulation.regulationStatistics.commentsCount - base.regulation.regulationStatistics.commentsCount;
+        regulationChange.regulationStatistics.constantsCount = target.regulation.regulationStatistics.constantsCount - base.regulation.regulationStatistics.constantsCount;
+        regulationChange.regulationStatistics.exceptionsCount = target.regulation.regulationStatistics.exceptionsCount - base.regulation.regulationStatistics.exceptionsCount;
+        regulationChange.regulationStatistics.flowControlsCount = target.regulation.regulationStatistics.flowControlsCount - base.regulation.regulationStatistics.flowControlsCount;
+        regulationChange.regulationStatistics.namingsCount = target.regulation.regulationStatistics.namingsCount - base.regulation.regulationStatistics.namingsCount;
+        regulationChange.regulationStatistics.oopsCount = target.regulation.regulationStatistics.oopsCount - base.regulation.regulationStatistics.oopsCount;
+        regulationChange.regulationStatistics.setsCount = target.regulation.regulationStatistics.setsCount - base.regulation.regulationStatistics.setsCount;
+        regulationChange.regulationStatistics.othersCount = target.regulation.regulationStatistics.othersCount - base.regulation.regulationStatistics.othersCount;
     }
 
     private void diffDesignChange(){
-
-
+        designChange.designStatistics.designsCount = target.design.designStatistics.designsCount - base.design.designStatistics.designsCount;
+        designChange.designStatistics.multithreadingsCount = target.design.designStatistics.multithreadingsCount - base.design.designStatistics.multithreadingsCount;
+        designChange.designStatistics.performancesCount = target.design.designStatistics.performancesCount - base.design.designStatistics.performancesCount;
     }
 
     //Maintenance, regulation, design only increase data
